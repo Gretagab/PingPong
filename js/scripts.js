c5f3762841966f1385923dc491828682e8d63d51
Greta <greta.gab@gmail.com>
2019-02-24 19:29:17 -0800

    JS file updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+  $("#submit").click(function() {
+    var destination = $("#paris");
+    if ($("#temperature1").prop('checked')) {
+      destination = $("#namibia");
+    } if ($("#fancy").prop('checked') && $("#familiar").prop('checked')) {
+      destination = $("#westcoast");
 
+    } else if ($("#plane-food").prop('checked')) {
+      destination = $("#westcoast");
+    }
 
-$(document).ready(function()
+    alert(destination.text());
+  });
+})
